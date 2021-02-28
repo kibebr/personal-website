a97a4ab3533439b2b8d18087466c3c27302ea817
kibebr <0763722@students.dadeschools.net>
2021-02-28 22:24:21 +0000

    modified eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,40 +1,44 @@
 const filters = require('./utils/filters.js')
 const transforms = require('./utils/transforms.js')
 const collections = require('./utils/collections.js')
+const svgContents = require('eleventy-plugin-svg-contents')
 
 module.exports = function (eleventyConfig) {
-    // Folders to copy to build dir (See. 1.1)
-    eleventyConfig.addPassthroughCopy("src/static");
+  eleventyConfig.addPassthroughCopy("src/static");
+  eleventyConfig.addPassthroughCopy("src/_assets/icons");
+  eleventyConfig.addPassthroughCopy("src/_assets/scripts");
+  eleventyConfig.addPassthroughCopy("src/_assets/videos");
+  eleventyConfig.addPassthroughCopy("src/_assets/images");
 
-    // Filters 
-    Object.keys(filters).forEach((filterName) => {
-        eleventyConfig.addFilter(filterName, filters[filterName])
-    })
+  // Filters 
+  Object.keys(filters.universalFilters).forEach(filter => eleventyConfig.addFilter(filter, filters.universalFilters[filter]))
+  Object.keys(filters.nunjucksAsyncFilters).forEach(filter => eleventyConfig.addNunjucksAsyncFilter(filter, filters.nunjucksAsyncFilters[filter]))
 
-    // Transforms
-    Object.keys(transforms).forEach((transformName) => {
-        eleventyConfig.addTransform(transformName, transforms[transformName])
-    })
+  // Transforms
+  Object.keys(transforms).forEach((transformName) => {
+    eleventyConfig.addTransform(transformName, transforms[transformName])
+  })
 
-    // Collections
-    Object.keys(collections).forEach((collectionName) => {
-        eleventyConfig.addCollection(collectionName, collections[collectionName])
-    })
+  // Collections
+  Object.keys(collections).forEach((collectionName) => {
+    eleventyConfig.addCollection(collectionName, collections[collectionName])
+  })
 
-    // This allows Eleventy to watch for file changes during local development.
-    eleventyConfig.setUseGitIgnore(false);
+  // This allows Eleventy to watch for file changes during local development.
+  eleventyConfig.setUseGitIgnore(false);
 
-    return {
-        dir: {
-            input: "src/",
-            output: "dist",
-            includes: "_includes",
-            layouts: "_layouts"
-        },
-        templateFormats: ["html", "md", "njk"],
-        htmlTemplateEngine: "njk",
+  // Inline svg
+  eleventyConfig.addPlugin(svgContents)
 
-        // 1.1 Enable eleventy to pass dirs specified above
-        passthroughFileCopy: true
-    };
+  return {
+    dir: {
+      input: "src/",
+      output: "dist",
+      includes: "_includes",
+      layouts: "_layouts"
+    },
+    templateFormats: ["html", "md", "njk"],
+    htmlTemplateEngine: "njk",
+    passthroughFileCopy: true
+  };
 };
